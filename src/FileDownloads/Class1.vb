fb6258a911d9e8d811dd58462f6b491659ebf616
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2019-12-24 16:13:40 -0500

    Add FileDownloads DLL

---
+++ src/FileDownloads/Class1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.HttpStyleUriParser
+Imports System.IO
+Imports System.Net.Http
+Imports System.Net
+
+
+Public Class Class1
+
+    Dim Installer_Run As New ProcessStartInfo
+    Dim Uninstaller_Run As New ProcessStartInfo
+
+    Public Sub DownloadFile(ByVal Exe_URL As String, ByVal Exe_Output As String)
+        If (Not System.IO.Directory.Exists("C:\Launcher")) Then
+            System.IO.Directory.CreateDirectory("C:\Launcher")
+        End If
+        If (Not System.IO.Directory.Exists("C:\Launcher\Downloads")) Then
+            System.IO.Directory.CreateDirectory("C:\Launcher\Downloads")
+        End If
+        Using client = New WebClient()
+            client.DownloadFile(New Uri(Exe_URL), "C:\Launcher\Downloads\" & Exe_Output & ".exe")
+        End Using
+    End Sub
+
+    Public Sub DownloadFileMSI(ByVal MSI_URL As String, ByVal MSI_Output As String)
+        If (Not System.IO.Directory.Exists("C:\Launcher")) Then
+            System.IO.Directory.CreateDirectory("C:\Launcher")
+        End If
+        If (Not System.IO.Directory.Exists("C:\Launcher\Downloads")) Then
+            System.IO.Directory.CreateDirectory("C:\Launcher\Downloads")
+        End If
+        Using client = New WebClient()
+            client.DownloadFile(New Uri(MSI_URL), "C:\Launcher\Downloads\" & MSI_Output & ".msi")
+        End Using
+    End Sub
+
+    Public Sub Install(ByVal FileName As String, ByVal Extention As String, ByVal Arguments As String)
+        Installer_Run.FileName = "C:\Launcher\Downloads\" & FileName & "." & Extention
+        Installer_Run.Arguments = Arguments
+        Installer_Run.UseShellExecute = True
+        Installer_Run.WindowStyle = ProcessWindowStyle.Normal
+        Dim proc As Process = Process.Start(Installer_Run)
+    End Sub
+
+    Public Sub Uninstall(ByVal CanBeUninstalled As Boolean, ByVal ProgramName As String, ByVal UninstallEXE As String, ByVal UninstallArguments As String)
+        If CanBeUninstalled = True Then
+            Uninstaller_Run.FileName = "C:\Launcher\Apps\" & ProgramName & UninstallEXE & ".exe"
+            Uninstaller_Run.Arguments = UninstallArguments
+            Uninstaller_Run.UseShellExecute = True
+            Uninstaller_Run.WindowStyle = ProcessWindowStyle.Normal
+            Dim proc As Process = Process.Start(Uninstaller_Run)
+        ElseIf CanBeUninstalled = False Then
+            Dim box = New UninstallFailed()
+            box.Show()
+            box.Text = ProgramName & " Failed to uninstall."
+            box.Label1.Text = ProgramName & " cannot be uninstalled. It has to be uninstalled manually."
+        End If
+    End Sub
+
+End Class
---
+++ src/FileDownloads/UninstallFailed.vb
@@ -0,0 +1,10 @@
+﻿Imports FileDownloads
+Public Class UninstallFailed
+    Private Sub UninstallFailed_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button1.Text = "OK"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
